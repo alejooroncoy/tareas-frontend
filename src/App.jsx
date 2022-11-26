357254318198085bda59c8f44401a95340fed056
alejooroncoy <adoa2705@gmail.com>
2022-11-26 11:00:35 -0500

    Change endpoints ✨´

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,71 @@
-import React from 'react';
-import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Table,Container,Button,Form} from 'react-bootstrap'
+import React from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Table, Container, Button, Form } from "react-bootstrap";
 
-class App extends React.Component{
-
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
-    this.state = ({
-      tareas : [],
-      descripcion : '',
-      estado : 'pendiente'
-    })
+    this.state = {
+      tareas: [],
+      descripcion: "",
+      estado: "pendiente",
+    };
     this.cambioDescripcion = this.cambioDescripcion.bind(this);
     this.guardar = this.guardar.bind(this);
     this.eliminar = this.eliminar.bind(this);
   }
 
-  cambioDescripcion(e){
-    //console.log(e.target.value);
+  cambioDescripcion(e) {
     this.setState({
-      descripcion : e.target.value
-    })
+      descripcion: e.target.value,
+    });
   }
 
-  guardar(e){
+  guardar(e) {
     e.preventDefault();
     const dataTarea = {
-      descripcion : this.state.descripcion,
-      estado : this.state.estado
-    }
-    //console.log(dataTarea)
-    //registro de nueva tarea
-    axios.post('https://cmayta-tareasapi.onrender.com/tarea',dataTarea)
-    .then(res =>{
-      console.log(res.data.content);
-      this.state.tareas.push(res.data.content)
-      var temp = this.state.tareas
-      this.setState({
-        descripcion:'',
-        tareas:temp
-      }).catch((error)=>{
-        alert(error.toString());
-      })
-    })
+      descripcion: this.state.descripcion,
+      estado: this.state.estado,
+    };
+    // registro de nueva tarea
+    axios
+      .post("https://tareas-backend.onrender.com//tarea", dataTarea)
+      .then((res) => {
+        console.log(res.data.content);
+        this.state.tareas.push(res.data.content);
+        var temp = this.state.tareas;
+        this.setState({
+          descripcion: "",
+          tareas: temp,
+        }).catch((error) => {
+          alert(error.toString());
+        });
+      });
   }
 
-  eliminar(cod){
-    axios.delete('https://cmayta-tareasapi.onrender.com/tarea/'+cod)
-    .then(res=>{
-      var temp = this.state.tareas.filter((tarea)=>tarea.id !== cod);
-      this.setState({
-        tareas:temp
-      })
-    })
+  eliminar(cod) {
+    axios
+      .delete("https://tareas-backend.onrender.com//tarea/" + cod)
+      .then((res) => {
+        var temp = this.state.tareas.filter((tarea) => tarea.id !== cod);
+        this.setState({
+          tareas: temp,
+        });
+      });
   }
-  
-  componentDidMount(){
-    console.log("cargando tareas...")
-    axios.get('https://cmayta-tareasapi.onrender.com/tarea')
-    .then(res=>{
+
+  componentDidMount() {
+    console.log("cargando tareas...");
+    axios.get("https://tareas-backend.onrender.com//tarea").then((res) => {
       console.log(res.data.content);
       this.setState({
-        tareas : res.data.content
-      })
-    })
+        tareas: res.data.content,
+      });
+    });
   }
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <Container>
           <h1>Lista de Tareas</h1>
@@ -81,22 +79,33 @@ class App extends React.Component{
               </tr>
             </thead>
             <tbody>
-              {this.state.tareas.map((tarea)=>{
-                return(
+              {this.state.tareas.map((tarea) => {
+                return (
                   <tr key={tarea.id}>
                     <td>{tarea.id}</td>
                     <td>{tarea.descripcion}</td>
                     <td>{tarea.estado}</td>
-                    <td><Button variant="danger" onClick={()=>this.eliminar(tarea.id)}>Eliminar</Button></td>
+                    <td>
+                      <Button
+                        variant="danger"
+                        onClick={() => this.eliminar(tarea.id)}
+                      >
+                        Eliminar
+                      </Button>
+                    </td>
                   </tr>
-                )
+                );
               })}
             </tbody>
           </Table>
           <h1>Agregar Tarea</h1>
           <Form onSubmit={this.guardar}>
             <Form.Group className="mb-3">
-              <Form.Control type="text" value={this.state.descripcion} onChange={this.cambioDescripcion} />
+              <Form.Control
+                type="text"
+                value={this.state.descripcion}
+                onChange={this.cambioDescripcion}
+              />
             </Form.Group>
             <Button variant="primary" type="submit">
               Guardar
@@ -104,7 +113,7 @@ class App extends React.Component{
           </Form>
         </Container>
       </div>
-    )
+    );
   }
 }
 
